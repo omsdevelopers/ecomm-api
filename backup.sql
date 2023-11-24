7576c3e759144115e17ef307aa80b4cd45ca2227
omsdev <orangemegasoftwaredev@gmail.com>
2023-11-24 04:41:17 -0800

    orders done

--- backup.sql
+++ backup.sql
@@ -41,22 +41,42 @@ REPLACE INTO `brands` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUE
 CREATE TABLE IF NOT EXISTS `carts` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint unsigned DEFAULT NULL,
+  `session_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `product_id` bigint unsigned NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `size` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `quantity` int DEFAULT NULL,
   `price` decimal(8,2) NOT NULL,
+  `total` decimal(20,6) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `carts_user_id_foreign` (`user_id`),
   KEY `carts_product_id_foreign` (`product_id`),
   CONSTRAINT `carts_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=277 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table ecommerce.carts: ~3 rows (approximately)
-REPLACE INTO `carts` (`id`, `user_id`, `product_id`, `name`, `size`, `quantity`, `price`, `created_at`, `updated_at`) VALUES
-	(192, 0, 20, 'Diary', '0', 2, 1.00, '2023-11-21 20:19:31', '2023-11-21 20:19:31');
+-- Dumping data for table ecommerce.carts: ~13 rows (approximately)
+REPLACE INTO `carts` (`id`, `user_id`, `session_id`, `product_id`, `name`, `size`, `quantity`, `price`, `total`, `created_at`, `updated_at`) VALUES
+	(231, 17, NULL, 22, 'Chocolate', '50', 1, 50.00, 50.000000, '2023-11-22 19:42:24', '2023-11-23 15:02:41'),
+	(238, 17, NULL, 21, 'Ghee', '30', 1, 2.00, 30.000000, '2023-11-22 19:45:55', '2023-11-23 15:02:41'),
+	(239, NULL, NULL, 21, 'Ghee', '30', NULL, 2.00, NULL, '2023-11-22 19:58:29', '2023-11-22 19:58:29'),
+	(240, NULL, NULL, 21, 'Ghee', '30', NULL, 2.00, NULL, '2023-11-22 20:02:44', '2023-11-22 20:02:44'),
+	(241, NULL, NULL, 21, 'Ghee', '30', NULL, 2.00, NULL, '2023-11-22 20:06:07', '2023-11-22 20:06:07'),
+	(242, NULL, NULL, 21, 'Ghee', '30', NULL, 2.00, NULL, '2023-11-22 20:06:10', '2023-11-22 20:06:10'),
+	(243, NULL, NULL, 21, 'Ghee', '200', NULL, 2.00, NULL, '2023-11-22 20:06:52', '2023-11-22 20:06:52'),
+	(244, NULL, NULL, 21, 'Ghee', '200', NULL, 2.00, NULL, '2023-11-22 20:07:16', '2023-11-22 20:07:16'),
+	(245, NULL, NULL, 21, 'Ghee', '30', NULL, 2.00, NULL, '2023-11-22 20:08:19', '2023-11-22 20:08:19'),
+	(246, NULL, '0', 21, 'Ghee', '30', NULL, 2.00, NULL, '2023-11-22 20:13:51', '2023-11-22 20:13:51'),
+	(261, 17, 'null', 22, 'Chocolate', '200', 1, 50.00, 200.000000, '2023-11-23 14:59:51', '2023-11-23 15:02:41'),
+	(262, 17, 'null', 21, 'Ghee', '200', 2, 2.00, 400.000000, '2023-11-23 15:01:16', '2023-11-23 15:02:41'),
+	(268, NULL, 'oshBGlMWQp7t0DTLGDKB4CWigGJzjU8If3KB9c33', 21, 'Ghee', '200', 3, 30.00, 600.000000, '2023-11-23 18:54:41', '2023-11-23 20:10:45'),
+	(269, NULL, 'oshBGlMWQp7t0DTLGDKB4CWigGJzjU8If3KB9c33', 22, 'Chocolate', '200', 2, 50.00, 400.000000, '2023-11-23 18:54:53', '2023-11-23 20:10:45'),
+	(271, NULL, 'i18uAKP2iVkNjvWGD3JWYww9NV0n44gPcuQNM8QJ', 22, 'Chocolate', '250', 5, 50.00, NULL, '2023-11-24 16:35:58', '2023-11-24 16:35:58'),
+	(272, NULL, 'i18uAKP2iVkNjvWGD3JWYww9NV0n44gPcuQNM8QJ', 20, 'Diary', '200', 3, 30.00, NULL, '2023-11-24 16:36:03', '2023-11-24 16:36:03'),
+	(274, NULL, 'i18uAKP2iVkNjvWGD3JWYww9NV0n44gPcuQNM8QJ', 21, 'Ghee', '30.00', 2, 30.00, NULL, '2023-11-24 16:36:30', '2023-11-24 16:36:30'),
+	(275, 1, 'null', 22, 'Chocolate', '200', NULL, 50.00, NULL, '2023-11-24 19:50:07', '2023-11-24 19:50:07'),
+	(276, 1, 'null', 20, 'Diary', '200', 3, 30.00, NULL, '2023-11-24 19:50:17', '2023-11-24 19:50:17');
 
 -- Dumping structure for table ecommerce.category
 CREATE TABLE IF NOT EXISTS `category` (
@@ -114,6 +134,41 @@ REPLACE INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 	(5, '2023_11_20_113409_create_carts_table', 2);
 
+-- Dumping structure for table ecommerce.orders
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `session_id` varchar(255) DEFAULT NULL,
+  `billing_first_name` varchar(255) DEFAULT NULL,
+  `billing_last_name` varchar(255) DEFAULT NULL,
+  `billing_company_name` varchar(255) DEFAULT NULL,
+  `billing_address` varchar(255) DEFAULT NULL,
+  `billing_apartment` varchar(255) DEFAULT NULL,
+  `billing_city` varchar(255) DEFAULT NULL,
+  `billing_state` varchar(255) DEFAULT NULL,
+  `billing_postcode` varchar(20) DEFAULT NULL,
+  `billing_email` varchar(255) DEFAULT NULL,
+  `billing_phone` varchar(20) DEFAULT NULL,
+  `order_notes` text,
+  `order_id` varchar(255) DEFAULT NULL,
+  `payment_method` varchar(50) DEFAULT NULL,
+  `razorpay_payment_id` varchar(255) DEFAULT NULL,
+  `order_status` enum('pending','processing','completed','cancelled','shipped') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'pending',
+  `product_id` text NOT NULL,
+  `quantity` int DEFAULT NULL,
+  `subtotal` decimal(20,6) DEFAULT NULL,
+  `payment_status` enum('paid','unpaid') DEFAULT 'unpaid',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table ecommerce.orders: ~3 rows (approximately)
+REPLACE INTO `orders` (`id`, `user_id`, `session_id`, `billing_first_name`, `billing_last_name`, `billing_company_name`, `billing_address`, `billing_apartment`, `billing_city`, `billing_state`, `billing_postcode`, `billing_email`, `billing_phone`, `order_notes`, `order_id`, `payment_method`, `razorpay_payment_id`, `order_status`, `product_id`, `quantity`, `subtotal`, `payment_status`, `created_at`, `updated_at`) VALUES
+	(7, NULL, 'i18uAKP2iVkNjvWGD3JWYww9NV0n44gPcuQNM8QJ', 'manu', 'sunil', 'Sri Dhanlakshmi Collection Silk', 'pinpaa', '13.278', 'kk', 'TN', '676565', 'port@gmail.com', '78786767675', 'New order', 'order_N4Hjtc4bNGiQ4x', 'Razorpay', NULL, 'pending', '["22","20","21"]', NULL, NULL, 'paid', '2023-11-24 18:52:09', '2023-11-24 19:39:06'),
+	(8, 17, 'null', 'asha', 'jini', 'Sri Dhanlakshmi Collection Silk', 'pinpaa', '13.278', 'kk', 'TN', '676565', 'port@gmail.com', '78786767675', 'Please carfeul', 'order_N4HnZgo8W56YvM', 'Razorpay', NULL, 'pending', '["22","21","22","21"]', NULL, NULL, 'paid', '2023-11-24 18:55:37', '2023-11-24 18:56:05'),
+	(9, 1, 'null', 'super', 'admin', 'naatumaadu', 'kanyakumari', 'DD complex', 'Nagercoil', 'TamilNadu', '629163', 'admin@mail.com', '9655804632', 'new one', 'order_N4IkUGCiHu16my', 'Razorpay', NULL, 'processing', '["22","20"]', NULL, NULL, 'paid', '2023-11-24 19:51:24', '2023-11-24 19:52:45');
+
 -- Dumping structure for table ecommerce.password_reset_tokens
 CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -156,14 +211,15 @@ CREATE TABLE IF NOT EXISTS `products` (
   `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `size` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table ecommerce.products: ~4 rows (approximately)
 REPLACE INTO `products` (`id`, `name`, `price`, `description`, `created_at`, `updated_at`, `category_id`, `brand_id`, `image`, `size`) VALUES
 	(17, 'Ghee', 3.00, 'New Ghee Varient', '2023-11-18 12:30:16', '2023-11-18 12:30:16', '1', '5', 'FROGkWb8c5rQmBOfVCmV.png', NULL),
 	(19, 'Milk', 2.00, 'Milk', '2023-11-18 17:04:05', '2023-11-18 17:04:05', '2', '3', 'fmA879QzFcGT1DthL56D.png', NULL),
-	(20, 'Diary', 1.00, 'new one', '2023-11-18 18:08:45', '2023-11-18 18:08:45', '2', '2', 'uCWtt0ZPEoB1g7IN2HKG.png', '500g:30, 1.5kg:200'),
-	(21, 'Ghee', 2.00, 'new one', '2023-11-18 18:47:37', '2023-11-18 18:47:37', '2', '2', 'ZjTAdCqJZv6v9npTXJm6.png', '500g:30, 1.5kg:200');
+	(20, 'Diary', 30.00, 'new one', '2023-11-18 18:08:45', '2023-11-18 18:08:45', '2', '2', 'uCWtt0ZPEoB1g7IN2HKG.png', '500g:30, 1.5kg:200'),
+	(21, 'Ghee', 30.00, 'new one', '2023-11-18 18:47:37', '2023-11-18 18:47:37', '2', '2', 'ZjTAdCqJZv6v9npTXJm6.png', '500g:30, 1.5kg:200'),
+	(22, 'Chocolate', 50.00, 'Chocolate', '2023-11-22 15:48:28', '2023-11-22 15:48:28', '2', '3', '6IkQqGtMLra5xtLDU8bv.jpg', '500g:50, 1.5kg:200, 2kg:250');
 
 -- Dumping structure for table ecommerce.users
 CREATE TABLE IF NOT EXISTS `users` (
